61912c0bbad0ff3183e87f4837613188d7841bbd
Ro-ma <roman.aladyev@xena.exchange>
2021-04-26 02:27:12 +0300

    update dns_recon.sh and Readme

--- README.md
+++ README.md
@@ -1,13 +1,35 @@
-##dns_recon.sh
-- DNS enumeration using amass/bruteforce/alterations
-- ASN getting (get_asn.sh)
-- CORS retrieving (get_cors.py)
-- taking screenshots of subdomains
+##dns_recon.sh - DNS enumeration using amass/bruteforce/alterations
+'bash dns_recon.sh -f <domains.txt> -p <project_name> -m full'
+'bash dns_recon.sh -d <domain.com> -p <project_name> -m full'
+
+Notes:
+- Need to provide correct pathes to tools and project folder - edit dns_recon.sh 
+- Also, would be good to provide api-keys for some services to amass in config.ini (check amass documentation and amass run line in dns_recon.sh)
+
+##get_asn.sh - ASN identification 
+'bash get_asn.sh -f $<domains.txt> > <output_asn.txt>'
+
+##get_cors.py - CORS retrieving
+'python3.7 get_cors.py --file <domains.txt> --output <output_folder>'
+
+##webscreenshot.py - screenshot taking
+'python3.7 <path_to_websceenshot_folder>/webscreenshot.py -i <domains.txt> -o <output_folder>'
+
+##flan - nmap scan with pretty reports
+'docker run -v $(CURDIR)/shared:/shared flan_scan -sC -Pn -p- -min-rate=400 --min-parallelism=512'
+$(CURDIR)/shared - this dir is dir on host machine and should contain ips.txt file, also it will contain report after the scan
+
+##gobuster - dir/vhost bruteforce
+TODO
 
 ##requiered tools
-- python3.7
-- amass
-- massdns
-- dnsgen
-- phantomjs
-- webscreenshot
+- python3.7 - to run several python scripts for brute-list building/reporting (sub_scripts folder, get_cors.py)  
+- amass - to collect subdomains from third party services (dns_recon.sh)
+- massdns - to resolve dns records (dns_recon.sh)
+- dnsgen - to create alterations using valid dns records (dns_recon.sh)  
+- phantomjs, webscreenshot - to screen domains
+- docker, flan - port scanning and reporting
+- go, gobuster - bruteforce dirs and vhost
+
+##Installation flow (clean Ubuntu 20)
+TODO
--- dns_recon.sh
+++ dns_recon.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+#Tools pathes
+AMASS_PATH=${HOME}/tools/amass_v3.4.2_linux_amd64
+MASSDNS_PATH=${HOME}/tools/massdns
+SCREEN_TOOL=${HOME}/tools/webscreenshot
+PROJECTS_FOLDER=${HOME}/myProjects
+
 run_for_only_one_domain(){
 	echo "domain is $domain and project is $project"
 	
@@ -7,7 +13,6 @@ run_for_only_one_domain(){
 	mkdir -p ${domain_folder}
 	
 	if [ "${scan_mode}" == "full" ]; then		
-
 
 		#PASSIVE SCAN
 
@@ -19,7 +24,6 @@ run_for_only_one_domain(){
 		echo "DNS resolving for amass + rapid7"
 		${MASSDNS_PATH}/bin/massdns -r data/public_dns.txt -q -t A -o S -w ${domain_folder}/resolved_passive.txt ${domain_folder}/passive_out.txt
 
-
 		#BRUTEFORCE
 
 		#Bruteforce list creation(brute_list.txt)
@@ -30,7 +34,6 @@ run_for_only_one_domain(){
 		echo "DNS resolving for brute list"
 		${MASSDNS_PATH}/bin/massdns -r data/public_dns.txt -q -t A -o S -w ${domain_folder}/resolved_brute.txt ${domain_folder}/brute_list.txt
 		rm ${domain_folder}/brute_list.txt
-
 
 		#ALTERATIONS BRUTEFORCE
 
@@ -44,30 +47,12 @@ run_for_only_one_domain(){
 		${MASSDNS_PATH}/bin/massdns -r data/public_dns.txt -q -t A -o S -w ${domain_folder}/resolved_alt.txt ${domain_folder}/alt_list.txt
 		rm ${domain_folder}/domains_for_alt.txt
 		rm ${domain_folder}/alt_list.txt
-
 		
 		#FINAL SUBDOMAIN REPORT
 		
 		#Build final list of subdomains(fin_only_domains.txt, fin_statistic.txt, fin_a_dns_records.txt)
 		echo "Fin report creation"
 		python3.7 sub_scripts/create_fin_report.py ${domain_folder} ${domain_folder}/resolved_passive.txt ${domain_folder}/resolved_brute.txt ${domain_folder}/resolved_alt.txt
-
-
-		#ASN ENUM
-
-		#Get ASN's for all subdomains(fin_asn.txt)
-		echo "Get ASN"
-		bash get_asn.sh -f ${domain_folder}/fin_only_domains.txt > ${domain_folder}/fin_asn.txt
-
-
-		#CORS RETRIEVING
-		#Get all cors headers for each subdomain
-		mkdir -p "${domain_folder}/cors"
-		python3.7 get_cors.py --file ${domain_folder}/fin_only_domains.txt --output ${domain_folder}/cors
-
-		#SCREENSHOTS FOR SUBDOMAINS
-		mkdir -p "${domain_folder}/screens"
-		python3.7 ${SCREEN_TOOL}/webscreenshot.py -i ${domain_folder}/fin_only_domains.txt -o ${domain_folder}/screens/
 
 	elif [ "${scan_mode}" == "scedule_scan" ]; then		
 		echo "Section under construction"
@@ -86,14 +71,6 @@ run_for_file(){
     echo "End of domainS scan"
 
 }
-
-#variables for setEnv.sh
-AMASS_PATH=${HOME}/tools/amass_v3.4.2_linux_amd64
-MASSDNS_PATH=${HOME}/tools/massdns
-SCREEN_TOOL=${HOME}/tools/webscreenshot
-PROJECTS_FOLDER=${HOME}/myProjects
-
-
 
 while getopts d:f:p:m: option
 do
