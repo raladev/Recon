8a54e49ed5576358f81037d44e0c82943b132720
Ro-ma <roman.aladyev@xena.exchange>
2021-06-01 19:58:33 +0300

    change regex for domain parsing

--- sub_scripts/create_domain_list_for_alt.py
+++ sub_scripts/create_domain_list_for_alt.py
@@ -7,8 +7,8 @@ if __name__ == "__main__":
     amass_out = sys.argv[2]
     brute_out = sys.argv[3]
 
-    amass_domain_list = [re.match("([a-z0-9-\.]*)\. ", i).group(1) for i in open(amass_out).read().splitlines()]
-    brute_domain_list = [re.match("([a-z0-9-\.]*)\. ", i).group(1) for i in open(brute_out).read().splitlines()]
+    amass_domain_list = [re.match("([a-z0-9-\._]*)\. ", i).group(1) for i in open(amass_out).read().splitlines()]
+    brute_domain_list = [re.match("([a-z0-9-\._]*)\. ", i).group(1) for i in open(brute_out).read().splitlines()]
 
     set_of_lists = set(amass_domain_list + brute_domain_list)
 
